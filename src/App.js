85b09b24c6d6ce58cf81d4f3a8e969a7f9f6b5e1
Yasmin Lopes <yasslopes2001@gmail.com>
2024-08-18 21:25:37 -0300

    atualizei para meu site

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-function MeuComponente() {
-  return (
-    <div>
-      <h1>Meu Título</h1>
-      <p>Meu primeiro parágrafo!</p>
-    </div>
-  );
-}
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MeuComponente />
+
+        <div>
+        <h1>Cats to relax</h1>
+        <p>Veja gatinhos fofos!</p>
+        </div>
+
         <hr />
-        <MeuComponente />
+        <div className="gif-container">
+          <img
+            src="https://i.pinimg.com/originals/3a/2a/a1/3a2aa11b218ddb328fb1277f2d91dbfa.gif"
+            alt="GIF animado"
+            className="gif-image"
+          />
+          <p>Gatinho dançando</p>
+        </div>
+
         <hr />
-        <MeuComponente />
+        <div className="gif-container">
+          <img
+            src="https://31.media.tumblr.com/f81af2c5c18950d411a4c6d4c39a5f06/tumblr_mvsx1npiXI1spnz0yo1_400.gif"
+            alt="GIF animado"
+            className="gif-image"
+          />
+          <p>Gatinho massagista</p>
+        </div>
+
+        <hr />
+
+        <div className="gif-container">
+          <img
+            src="https://www.essaseoutras.com.br/wp-content/uploads/2011/11/gatinhos-gif-animado.gif"
+            alt="GIF animado"
+            className="gif-image"
+          />
+          <p>Gatinho lutador</p>
+        </div>
+
+        <hr />
+
       </header>
     </div>
   );
 }
 
 export default App;
+
